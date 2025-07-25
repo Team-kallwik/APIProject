fa3737d016560940f296fc69bbc815c651a9f544
Akshitasharma879 <bajpaivaishnavi176@gmail.com>
2025-07-25 15:58:16 +0530

    first commit

---
+++ Vaishnavi/WithoutAuthentication/Models/Emp.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperConfig.Models
+{
+    public class Emp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+    }
+}
