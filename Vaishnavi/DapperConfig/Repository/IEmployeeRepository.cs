152955a269da85543c2e9e3aa6b4d54f35720c39
Akshitasharma879 <bajpaivaishnavi176@gmail.com>
2025-07-25 12:35:09 +0530

    first commit

---
+++ Vaishnavi/DapperConfig/Repository/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using DapperConfig.Models;
+
+
+namespace DapperConfig.Repository
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Emp>> GetAllAsync();
+        Task<Emp> GetByIdAsync(int id);
+        Task<int> AddAsync(Emp employee);
+        Task<int> UpdateAsync(Emp employee);
+        Task<int> DeleteAsync(int id);
+    }
+}
