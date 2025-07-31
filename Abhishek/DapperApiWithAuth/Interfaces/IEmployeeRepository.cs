d016d276c0856c1e88defda57b57adb8dcd23c80
Abhirays <abhi47200247@gmail.com>
2025-07-31 16:24:16 +0530

    Initial commit

---
+++ Abhishek/DapperApiWithAuth/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using DapperApiWithAuth.DTOs;
+
+namespace DapperApiWithAuth.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<EmployeeDto>> GetAllAsync();
+        Task<EmployeeDto> GetByIdAsync(int id);
+        Task AddBulkAsync(IEnumerable<EmployeeDto> employees);
+        Task UpdateAsync(EmployeeDto emp);
+        Task DeleteAsync(int id);
+    }
+}
