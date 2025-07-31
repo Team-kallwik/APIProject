e2ae3a7c98ea4f7d216b39771a2e03e6e7296bfc
paras-kallwik <parasvanve218@gmail.com>
2025-07-31 16:52:21 +0530

    .

--- Paras/DapperWebApiWIthAuthentication/DapperWebApiWIthAuthentication/Models/LoginEmployee.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DapperWebApiWIthAuthentication.Models
-{
-    public class LoginEmployee
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
