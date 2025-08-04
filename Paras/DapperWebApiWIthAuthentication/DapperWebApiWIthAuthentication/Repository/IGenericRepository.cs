2c0b4808551fd0f7223539231a09f4cb31dd2502
paras-kallwik <parasvanve218@gmail.com>
2025-08-04 15:30:57 +0530

    add Generic repository

---
+++ Paras/DapperWebApiWIthAuthentication/DapperWebApiWIthAuthentication/Repository/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DapperWebApiWIthAuthentication.Repository
+{
+    public interface IGenericRepository<T>
+    {
+        Task<string> GetAllAsync(string storedProcedure);
+        Task<T> GetByIdAsync(string storedProcedure, int id);
+        Task CreateAsync(string storedProcedure, object dto);
+        Task<bool> UpdateAsync(string storedProcedure, T entity);
+        Task<bool> DeleteAsync(string storedProcedure, int id);
+    }
+}
